ee7f3d8b1bf9f27e2f5062211fd87c1f0a9b55a2
hansenhy89 <47613647+hansenhy89@users.noreply.github.com>
2022-01-23 21:35:39 -0500

    Update greetings.js

--- routes/textbooks/booka1/a1js/greetings.js
+++ routes/textbooks/booka1/a1js/greetings.js
@@ -4,14 +4,14 @@ const parentDiv = document.getElementById('tab-container');
 const tabs = ['contractions', 'spelling', 'nationality', 'age', 'grammar'];
 
 
-//CREATE DIVS
-for(let i = 0; i < tabs.length; i++){
-    const tablinks = document.createElement('div');
-    tablinks.id = tabs[i];
-    tablinks.classList = 'tablinks'
-    tablinks.innerHTML = tablinks.id;
-    parentDiv.appendChild(tablinks);
-};
+// //CREATE DIVS
+// for(let i = 0; i < tabs.length; i++){
+//     const tablinks = document.createElement('div');
+//     tablinks.id = tabs[i];
+//     tablinks.classList = 'tablinks'
+//     tablinks.innerHTML = tablinks.id;
+//     parentDiv.appendChild(tablinks);
+// };
 
 //CREATE CLASS ON ARRAY ITEMS
 let element = document.querySelectorAll('.tablinks');
