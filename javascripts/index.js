aff68e77570fac45a79e916c2206b07b3c03d848
beehiveEducation <47613647+beehiveEducation@users.noreply.github.com>
2022-04-29 17:59:35 -0500

    Update index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -7,17 +7,21 @@ initHeader();
 function initHeader() {
     headerLinks.forEach(x => {
         x.addEventListener('click', function (e) {
+    let id = e.target.classList.value;
+    const classes = id.split(' ');
+    individualClass = classes.find(x => x != link);
+            if(individualClass.includes('home'){
+               loadHomePage(e);
+               }
             loadpage(e);  
         })
     })
 }
 
+function loadHomePage() {
+    window.location.href = "/textbooks/";
+}
+
 function loadpage(e) {
-    let id = e.target.classList.value;
-    const classes = id.split(' ');
-    individualClass = classes.find(x => x != link);
-    if(individualClass.includes('button')) {
-       window.location.href = "/textbooks/"
-       }
     window.location.href = `/textbooks/routes/${individualClass}.html`;
 }
