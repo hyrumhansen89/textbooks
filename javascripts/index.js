7ad1037517a385ca76934a0a9e7e5f74c0ded989
beehiveEducation <47613647+beehiveEducation@users.noreply.github.com>
2022-04-28 21:11:42 -0500

    Create index.js

---
+++ javascripts/index.js
@@ -0,0 +1,6 @@
+let homebtn = document.querySelector('homeDiv, logo, logoParagraph');
+
+homebtn.addEventListener('click', function(){
+  location.href = "https://beehiveeducation.github.io/textbooks/";
+  
+})
