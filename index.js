bc70bfeeb77e2ee4aa00644878ebfef6c32ba26c
beehiveEducation <47613647+beehiveEducation@users.noreply.github.com>
2022-04-29 09:47:44 -0500

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT;
-
-app.get('/', (req, res) => res.send('hello world'));
-
-app.listen(port, () => console.log('app listening at http://localhost:${port}));
